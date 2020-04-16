dd72d7d2531c4c8a9074d6f01e4e57aeff3f43b6
dushandz <dushandz@gmail.com>
2020-04-16 22:41:43 +0800

    Initial Commit

---
+++ DZPokemon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DZPokemon
+//
+//  Created by dushandz on 2020/4/16.
+//  Copyright © 2020 dushandz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
